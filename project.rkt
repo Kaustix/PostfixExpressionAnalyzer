6315220404a3d15d1b197ea13e2194c18958793e
Kiernan Lamarche <kiernan.lamarche@gmail.com>
2014-04-04 21:30:48 -0600

    Update project.rkt
    add keyboard input function and string split function

--- project.rkt
+++ project.rkt
@@ -3,44 +3,70 @@
 
 (define (main)
   (display "UofL>")
-
   
-  (let ((expr (read)))
-    (if (equal? expr 'stop)
-        'okay
-        (begin (display expr) (newline) (main))
-    )
-   )
+  (define inputString(read-keyboard-as-string)) ; get the input string
+  (define inputList(str-split a #\space)) ; convert the input string to a list
+  
+  (interpret inputList) ; interpret the input list
+
+  (main) ; recursivly call main
 )
 
-;PUSH
-(define (push arg) (set! stackList (cons arg stackList)))
+(define (interpret arg)
+  (if (
+  
+)
 
-;POP
-(define (pop) 
+(define (read-keyboard-as-string) ; this function returns keyboard input as a string
+  (let ((char (read-char)))
+    (if (char=? char #\newline)
+        '()
+        (list->string
+         (let loop ((char char))
+           (if (char=? char #\newline)
+               '()
+               (cons char (loop (read-char)))
+               )
+           )
+         )
+        )
+    )
+)
+
+(define (str-split str ch) ; this function splits a string by a character and returns a list
+  (let ((len (string-length str)))
+    (letrec
+      ((split
+        (lambda (a b)
+          (cond
+            ((>= b len) (if (= a b) '() (cons (substring str a b) '())))
+            ((char=? ch (string-ref str b)) (if (= a b)
+                                              (split (+ 1 a) (+ 1 b))
+                                              (cons (substring str a b) (split b b))))
+            (else (split a (+ 1 b)))))))
+      (split 0 0))))
+
+(define (push arg) (set! stackList (cons arg stackList))) ; pushes item onto a global stack list
+
+(define (pop) ; sets the temp variable to top item on the stack
   (set! temp (car stackList))
   (set! stackList (cdr stackList))
 )
 
-;STACK
-(define (stack) (display-stack stackList))
-(define (display-stack list) 
+(define (stack) (display-list stackList)) ; displays the stack list as seperate string values
+(define (display-list list) 
   (if (not(null? list)) 
-      (begin (display (car list)) (display #\space) (display-stack (cdr list)))
+      (begin (display (car list)) (display #\space) (display-list (cdr list)))
   )
 )
 
-;DROP
-(define (drop) (set! stackList (cdr stackList)))
+(define (drop) (set! stackList (cdr stackList))) ; removes the top item of the stack
 
-;SAVE
-(define (save) (push temp))
+(define (save) (push temp)) ; pushes the current item in temp to the top of the stack
 
-;DUP
-(define (dup) (push (car stackList)))
+(define (dup) (push (car stackList))) ; duplicates the first item on the stack
 
-;SWAP
-(define (swap)
+(define (swap) ; swaps the first and second item on the stack
   (pop)
   (let ((first-element temp))
     (pop)
@@ -51,7 +77,4 @@
   )
 )
 
-;CLEAR
-(define (clear) (set! stackList '()))
-  
-                
+(define (clear) (set! stackList '())) ; resets the stack to an empty list
